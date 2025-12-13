e53aa6648f52d6a90fb207d91dd0b54d5eab0895
alexk11 <akryuchkov1@gmail.com>
2025-12-13 13:36:09 +0300

    removed unneeded annotation in the service

--- payment-service-app/src/main/java/com/iprody/mapper/PaymentMapper.java
+++ payment-service-app/src/main/java/com/iprody/mapper/PaymentMapper.java
@@ -2,12 +2,8 @@ package com.iprody.mapper;
 
 import com.iprody.model.PaymentDto;
 import com.iprody.persistence.PaymentEntity;
-import org.mapstruct.AnnotateWith;
 import org.mapstruct.Mapper;
-import org.springframework.stereotype.Service;
 
-
-@AnnotateWith(value = Service.class, elements = @AnnotateWith.Element(strings = "mapper"))
 @Mapper(componentModel = "spring")
 public interface PaymentMapper {
     PaymentDto toPaymentDto(PaymentEntity paymentEntity);
