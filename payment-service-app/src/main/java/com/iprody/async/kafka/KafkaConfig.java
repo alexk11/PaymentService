70b966c5ac35e6d5d9fef01c3fbe899e7d67ff1b
Alexey Kryuchkov <akryuchkov1@gmail.com>
2026-01-20 09:32:08 +0300

    changed kafka host in config bean

--- payment-service-app/src/main/java/com/iprody/async/kafka/KafkaConfig.java
+++ payment-service-app/src/main/java/com/iprody/async/kafka/KafkaConfig.java
@@ -1,11 +1,11 @@
 package com.iprody.async.kafka;
 
-import com.fasterxml.jackson.databind.JsonSerializer;
 import com.iprody.async.XPaymentAdapterRequestMessage;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.support.serializer.JacksonJsonSerializer;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
@@ -13,52 +13,22 @@ import org.springframework.kafka.core.ProducerFactory;
 import java.util.HashMap;
 import java.util.Map;
 
+
 @Configuration
 public class KafkaConfig {
 
     @Bean
     public ProducerFactory<String, XPaymentAdapterRequestMessage> producerFactory() {
         Map<String, Object> config = new HashMap<>();
-        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092");
         config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JacksonJsonSerializer.class);
         return new DefaultKafkaProducerFactory<>(config);
     }
 
     @Bean
     public KafkaTemplate<String, XPaymentAdapterRequestMessage> kafkaTemplate() {
-        return new KafkaTemplate<String, XPaymentAdapterRequestMessage>(producerFactory());
+        return new KafkaTemplate<>(producerFactory());
     }
-
-//    public ProducerFactory<String, ServingDetailsEntity> producerFactoryServingDetail() {
-//        Map<String, Object> config = new HashMap<>();
-//        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
-//        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-//        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
-//        return new DefaultKafkaProducerFactory(config);
-//    }
-//
-//    @Bean
-//    public KafkaTemplate<String, ServingDetailsEntity> kafkaTemplateServingDetailsListener() {
-//        return new KafkaTemplate<String, ServingDetailsEntity>(producerFactoryServingDetail());
-//    }
-//
-//    @Bean
-//    public ConsumerFactory<String, ServingDetailsEntity> consumerFactory() {
-//        Map<String, Object> config = new HashMap<>();
-//        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
-//        config.put(ConsumerConfig.GROUP_ID_CONFIG, "event-group");
-//        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringSerializer.class);
-//        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonSerializer.class);
-//        return new DefaultKafkaConsumerFactory<>(config, new StringDeserializer(),
-//                new JsonDeserializer<>(ServingDetailsEntity.class));
-//    }
-//
-//    @Bean
-//    public ConcurrentKafkaListenerContainerFactory<String, ServingDetailsEntity> kafkaTemplateTaskDetailsListener() {
-//        ConcurrentKafkaListenerContainerFactory<String, ServingDetailsEntity> factory = new ConcurrentKafkaListenerContainerFactory<String, ServingDetailsEntity>();
-//        factory.setConsumerFactory(consumerFactory());
-//        return factory;
-//    }
 
 }
