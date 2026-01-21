5fd2de181b4f0b1db56b7854147c7d940a8da572
Alexey Kryuchkov <akryuchkov1@gmail.com>
2026-01-21 10:24:39 +0300

    created payment-service-api module

---
+++ payment-service-app/src/main/java/com/iprody/async/RequestMessageHandler.java
@@ -0,0 +1,51 @@
+package com.iprody.async;
+
+import com.iprody.AsyncSender;
+import com.iprody.XPaymentAdapterStatus;
+import com.iprody.dto.XPaymentAdapterRequestMessage;
+import com.iprody.dto.XPaymentAdapterResponseMessage;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import jakarta.annotation.PreDestroy;
+import org.springframework.stereotype.Component;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+
+@Slf4j
+@Component
+public class RequestMessageHandler implements MessageHandler<XPaymentAdapterRequestMessage> {
+
+    private final AsyncSender<XPaymentAdapterResponseMessage> sender;
+    private final ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
+
+    @Autowired
+    public RequestMessageHandler(AsyncSender<XPaymentAdapterResponseMessage> sender) {
+        this.sender = sender;
+    }
+
+    @Override
+    public void handle(XPaymentAdapterRequestMessage message) {
+        scheduler.schedule(() -> {
+            XPaymentAdapterResponseMessage responseMessage = new XPaymentAdapterResponseMessage();
+            responseMessage.setPaymentGuid(message.getPaymentGuid());
+            responseMessage.setAmount(message.getAmount());
+            responseMessage.setCurrency(message.getCurrency());
+            responseMessage.setStatus(XPaymentAdapterStatus.SUCCEEDED);
+            responseMessage.setTransactionRefId(UUID.randomUUID());
+            responseMessage.setOccurredAt(OffsetDateTime.now());
+
+            log.info("In handle, sending response message {}", responseMessage);
+            sender.send(responseMessage);
+        }, 10, TimeUnit.SECONDS);
+    }
+
+    @PreDestroy
+    public void shutdown() {
+        scheduler.shutdown();
+    }
+
+}
--- payment-service-app/src/main/java/com/iprody/async/kafka/KafkaXPaymentAdapterRequestListenerAdapter.java
+++ payment-service-app/src/main/java/com/iprody/async/kafka/KafkaXPaymentAdapterRequestListenerAdapter.java
@@ -2,6 +2,7 @@ package com.iprody.async.kafka;
 
 import com.iprody.AsyncListener;
 import com.iprody.async.MessageHandler;
+import com.iprody.dto.XPaymentAdapterRequestMessage;
 import com.iprody.dto.XPaymentAdapterResponseMessage;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
@@ -12,33 +13,30 @@ import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
-public class KafkaXPaymentAdapterResultListenerAdapter
-        implements AsyncListener<XPaymentAdapterResponseMessage> {
+public class KafkaXPaymentAdapterRequestListenerAdapter
+        implements AsyncListener<XPaymentAdapterRequestMessage> {
 
-    private final MessageHandler<XPaymentAdapterResponseMessage> handler;
+    private final MessageHandler<XPaymentAdapterRequestMessage> handler;
 
-    public KafkaXPaymentAdapterResultListenerAdapter(
-            MessageHandler<XPaymentAdapterResponseMessage> handler) {
+    public KafkaXPaymentAdapterRequestListenerAdapter(
+            MessageHandler<XPaymentAdapterRequestMessage> handler) {
         this.handler = handler;
     }
 
     @Override
-    public void onMessage(XPaymentAdapterResponseMessage message) {
+    public void onMessage(XPaymentAdapterRequestMessage message) {
         handler.handle(message);
     }
 
-    @KafkaListener(topics =
-            "${app.kafka.topics.xpayment-adapter.response}",
+    @KafkaListener(topics = "${app.kafka.topics.x-payment-adapter.request}",
             groupId = "${spring.kafka.consumer.group-id}")
-
     public void consume(
-            XPaymentAdapterResponseMessage message,
-            ConsumerRecord<String, XPaymentAdapterResponseMessage> record,
+            XPaymentAdapterRequestMessage message,
+            ConsumerRecord<String, XPaymentAdapterRequestMessage> record,
             Acknowledgment ack) {
         try {
-            log.info("Received XPayment Adapter response: paymentGuid={}, status={}, partition={}, offset={}",
+            log.info("Received XPayment Adapter request: paymentGuid={}, partition={}, offset={}",
                     message.getPaymentGuid(),
-                    message.getStatus(),
                     record.partition(),
                     record.offset());
             onMessage(message);
